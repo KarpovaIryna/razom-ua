9f281b884ff91d1f07a9f31aa03610b6e3f8653b
Karpova <karpovai@outlook.com>
2020-09-25 23:39:06 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	lintOnSave: false,
-	publicPath: process.env.NODE_ENV === 'production' ? '/razom-ua/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' 
+	? '/razom-ua/' 
+	: '/'
 }
